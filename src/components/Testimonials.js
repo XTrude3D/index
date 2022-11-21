a47ba69d9dcdec7bc8bb5db894883b4b7d66cb36
iszakpeter00 <iszakpeter00@gmail.com>
2022-11-21 17:56:50 +0100

    Price changes

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -14,19 +14,19 @@ const Testimonials = () => {
                     <div className='card'>
                         {/* <img src={user1} alt='user1'/> */}
                         {/* <p>orál 1000</p> */}
-                        <h1><span>orál</span></h1>
+                        <h1><span>Termék1</span></h1>
                         <p>1000ft</p>
                     </div>
                     <div className='card'>
                         {/* <img src={user2} alt='user1'/>
                         <p>In just 2 very short meetings with John he managed to find the solution personally catered to my situation and expectations. Punctual, well informed and very reliable.</p> */}
-                        <h1><span>natúr</span></h1>
+                        <h1><span>Termék2</span></h1>
                         <p>2000ft</p>
                     </div>
                     <div className='card'>
                         {/* <img src={user3} alt='user1'/>
                         <p>A very professional financial advisor, who is true to his word. John has demonstrated a high amount of integrity in the time I have known him, and he is fast to respond to my concerns.</p> */}
-                        <p><span>hátulról</span></p>
+                        <p><span>Termék3</span></p>
                         <p>4000ft</p>
                     </div>
                 </div>
