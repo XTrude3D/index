5f1109e5aa9b26968ce9bffbebece2ab86423ecb
iszakpeter00 <iszakpeter00@gmail.com>
2022-11-27 20:09:49 +0100

    Video fixed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './Home.css'
-import background from './images/tesztvideo2.mp4'
+import background from './images/tesztvideo.mp4'
 
 const Home = () => {
     return (
         <div className='home' id='home'>
-            <video src={background} autoPlay muted loop/>
+            <video src={background} autoPlay muted loop playsInline/>
             <div className='content'>
                 <h1><span>X</span>TRUDE</h1>
                 <h2><span>3D</span> NYOMTATÁS ÉS TERVEZÉS</h2>
