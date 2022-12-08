41e9107434e0815586d3c558e4043583aae85fed
iszakpeter00 <iszakpeter00@gmail.com>
2022-12-08 15:46:50 +0100

    Form added and sends data to e-mail

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Contact.css'
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_84bsd2i', 'template_9uxm7pb', form.current, '4EgQR1w60P5j1gi5b')
+            .then((result) => {
+                console.log(result.text);
+                alert("Köszönjük megkeresését, üzenetét elküldtük!");
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+
     return (
         <div className='contact' id='contact'>
             <div className='background'>
@@ -12,11 +28,29 @@ const Contact = () => {
                     <p>Eltört egy műanyag alkatrészed és szeretnéd pótolni?</p>
                     <p>Esetleg egy egyedi dologra van szükséged?</p>
                     <p>Keress minket bátran és segítünk!</p>
-                    
-                </div>
 
+                    <form ref={form} onSubmit={sendEmail}>
+                        <label>Név</label>
+                        <input type="text" name="name" placeholder="Név" required />
+                        <label>E-mail</label>
+                        <input type="text" name="email" placeholder="E-mail" required />
+                        <label>Termék</label>
+                        <select name="product" required>
+                            <option>Termék1</option>
+                            <option>Termék2</option>
+                            <option>Termék3</option>
+                            <option>Termék4</option>
+                            <option>Termék5</option>
+                            <option>Információt kérek</option>
+                        </select>
+                        <label>Üzenet</label>
+                        <textarea name="message" placeholder="Üzenet" required></textarea>
+                        <input className="button" type="submit" value="Küldés" />
+                    </form>
+                </div>
             </div>
         </div>
+
     )
 }
 
