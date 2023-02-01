e48b83b3bdacd449268fbea86a01c68ecbe50d10
iszakpeter00 <iszakpeter00@gmail.com>
2023-02-01 11:03:30 +0100

    Responsive design

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import './Home.css'
 import background from './images/ujvideo.mp4'
+import mobile from './images/mobile.jpg'
 
 const Home = () => {
     return (
-        <div className='home' id='home'>
-            <video src={background} autoPlay muted playsInline/>
-            {/* <div className='content'>
-                <h1><span>X</span>TRUDE</h1>
-                <h2><span>3D</span> NYOMTATÁS ÉS TERVEZÉS</h2>
-            </div> */}
+        <div className='container'>
+            <div className='blank'></div>
+            <div className='mobile'>
+                <img src={mobile} alt="xtrude"></img>
+            </div>
+            <div className='home' id='home'>
+                <video src={background} autoPlay muted playsInline/>
+                {/* <div className='content'>
+                    <h1><span>X</span>TRUDE</h1>
+                    <h2><span>3D</span> NYOMTATÁS ÉS TERVEZÉS</h2>
+                </div> */}
+            </div>
         </div>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -33,14 +33,14 @@ const Navbar = () => {
                     {click ? (<FaTimes size={30} style={{ color: '#ffffff' }} />)
                         : (<FaBars size={30} style={{ color: '#ffffff' }} />)}
                 </div>
-                <div className={click ? "nav-menu active" : "nav-menu"}>
+                <ul className={click ? "nav-menu active" : "nav-menu"}>
                     <Link className='nav-item' activeClass='current' to="home" spy={true} smooth={true} offset={0} duration={500} onClick={closeMenu}>Kezdőlap</Link>
                     <Link className='nav-item' activeClass='current' to="about" spy={true} smooth={true} offset={20} duration={500} onClick={closeMenu}>Rólunk</Link>
                     <Link className='nav-item' activeClass='current' to="services" spy={true} smooth={true} offset={20} duration={500} onClick={closeMenu}>Szolgáltatások</Link>
                     <Link className='nav-item' activeClass='current' to="products" spy={true} smooth={true} offset={20} duration={500} onClick={closeMenu}>Mintatermékek</Link>
                     <Link className='nav-item' activeClass='current' to="contact" spy={true} smooth={true} offset={20} duration={800} onClick={closeMenu}>Kapcsolat</Link>
                     <Link className='nav-item' activeClass='current' to="footer" spy={true} smooth={true} offset={20} duration={500} onClick={closeMenu}>Akadémia</Link>            
-                </div>
+                </ul>
             </nav>
         </div>
     )
